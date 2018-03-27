d1664f14b745357a704f8eb28e243c5ea1dab16a
Fagner <fagner.silva.martins@gmail.com>
2018-03-26 21:06:02 -0300

    New layout for tablet

--- js/api.js
+++ js/api.js
@@ -24,18 +24,8 @@
     function success(data) {
         var divNews = $('#news');
         divNews.empty();
-        //setTopNews( data.articles[0]);
         for (var i = 0; i < data.articles.length; ++i) {
             divNews.append(getNewsHtml(data.articles[i]));
-        }
-    }
-
-    function setTopNews(article) {
-        if (article) {
-            $('#top-news-title').text(article.title);
-            $('#top-news-description').text(article.description);
-            $('#top-news-image').attr('src', article.urlToImage).attr('alt', article.title);
-            $('#top-news-link').attr('href', article.url);
         }
     }
 
@@ -73,7 +63,7 @@
     function activeMenu(menu) {
         search = null;
         $("#search").val('');
-        $('li.active').removeClass('active');
+        $('button.active').removeClass('active');
         menu.addClass('active');
         getNews();
     }
@@ -94,40 +84,27 @@
 
     function getNewsHtml(article) {
 
-        var card = $('<div>').addClass('card row col-xs-12 col-md-6');
+        var card = $('<div>').addClass('card row col-xs-12 col-sm-12 col-md-6');
 
-        //card = addImage(card);
         card = addBodyTitle(card);
         card = addDescription(card);
         card = addBodyActions(card);
 
         return card;
 
-        function addImage(card) {
-            if (article.urlToImage) {
-                return card.append(
-                    $('<img>')
-                        .attr('src', article.urlToImage)
-                        .attr('alt', article.title)
-                        .addClass('card-img-top')
-                );
-            }
-            return card;
-        }
-
         function addBodyTitle(card) {
             return card
                 .append(
                     $('<div>')
-                        .addClass('col-xs-8 col-md-8')
+                        .addClass('col-xs-8 col-sm-8 col-md-8')
                         .append($('<h4>').addClass('card-title').append(article.title))
                 )
                 .append(
                     $('<div>')
-                        .addClass('col-xs-4 col-md-4')
+                        .addClass('col-xs-4 col-sm-4 col-md-4')
                         .append(
                             $('<img>')
-                                .attr('src', article.urlToImage? article.urlToImage: 'image/placeholder-image.png')
+                                .attr('src', article.urlToImage ? article.urlToImage : 'image/placeholder-image.png')
                                 .attr('alt', article.title)
                                 .addClass('card-img-top')
                         )
@@ -138,7 +115,7 @@
             return card
                 .append(
                     $('<div>')
-                        .addClass('col-xs-12 hide-xs')
+                        .addClass('col-xs-12 col-sm-12 hide-xs')
                         .append($('<p>').addClass('card-text').append(article.description))
                 );
         }
@@ -146,13 +123,12 @@
         function addBodyActions(card) {
             return card.append(
                 $('<div>')
-                    .addClass('col-xs-12 col-md-12')
+                    .addClass('col-xs-12 col-sm-12 col-md-12')
                     .append($('<h6>').addClass('col-6 card-date')
                         .append(moment(article.publishedAt).fromNow()))
                     .append($('<button>')
-                        .append('<i class="material-icons">keyboard_arrow_down</i>')
-                        .append('Show more')
-                        .addClass('card-button float-right hide-md')
+                        .append('Read article')
+                        .addClass('card-button float-right bt-default')
                         .attr('type', 'button'))
                     .click(function () {
                         window.open(article.url, '_blank');
